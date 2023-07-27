ba0a66f95743e21e4f3363eda4ece8fa0985d1fc
daengdaengLee <gunho1020@gmail.com>
2023-07-27 11:27:01 +0900

    test: debounce stream 테스트 작성

---
+++ src/debounce-stream.test.ts
@@ -0,0 +1,49 @@
+import { Readable, Writable, WritableOptions } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { DebounceStream } from "./debounce-stream.js";
+import { delay } from "./util.js";
+
+class ToArrayStream extends Writable {
+  constructor(
+    private readonly acc: Array<any>,
+    options?: WritableOptions,
+  ) {
+    super(options);
+  }
+
+  _write(
+    chunk: unknown,
+    _encoding: BufferEncoding,
+    callback: (error?: Error | null) => void,
+  ) {
+    this.acc.push(chunk);
+    callback();
+  }
+}
+
+describe(`DebounceStream Test`, () => {
+  it(`only the last value remains among the consecutive values.`, async () => {
+    const waitMs = 500;
+
+    const outputData: Array<Buffer> = [];
+    await pipeline(
+      Readable.from(
+        (async function* () {
+          yield Buffer.from([1]);
+          yield Buffer.from([2]);
+          yield Buffer.from([3]);
+
+          await delay(waitMs * 2);
+
+          yield Buffer.from([4]);
+          yield Buffer.from([5]);
+          yield Buffer.from([6]);
+        })(),
+      ),
+      new DebounceStream(waitMs),
+      new ToArrayStream(outputData),
+    );
+
+    expect(outputData).toEqual([Buffer.from([3]), Buffer.from([6])]);
+  });
+});
