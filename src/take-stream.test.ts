171e45c20e09a6eb9d06e8d47f877866da5cf9d1
daengdaengLee <gunho1020@gmail.com>
2023-07-28 22:41:39 +0900

    test: take stream 테스트

---
+++ src/take-stream.test.ts
@@ -0,0 +1,27 @@
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { TakeStream } from "./take-stream.js";
+import { ToArrayStream } from "./to-array-stream.js";
+
+describe(`TakeStream Test`, () => {
+  it(`outputs only the number you set and exits.`, async () => {
+    const n = 2;
+
+    const inputs = [
+      Buffer.from([1]),
+      Buffer.from([2]),
+      Buffer.from([3]),
+      Buffer.from([4]),
+      Buffer.from([5]),
+      Buffer.from([6]),
+    ];
+    const outputs: Array<Buffer> = [];
+    await pipeline(
+      Readable.from(inputs),
+      new TakeStream({ n: n }),
+      new ToArrayStream({ target: outputs }),
+    );
+
+    expect(outputs).toEqual(inputs.slice(0, n));
+  });
+});
