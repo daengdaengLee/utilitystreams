b0a1d9b46b59b9064c1e9e5f87bc850d1262ea27
daengdaengLee <gunho1020@gmail.com>
2023-07-31 14:25:12 +0900

    test: 테스트 작성

---
+++ src/buffer-stream.test.ts
@@ -0,0 +1,70 @@
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { BufferStream } from "./buffer-stream.js";
+import { ToArrayStream } from "./to-array-stream.js";
+import { delay } from "./util.js";
+
+describe(`BufferStream Test`, () => {
+  it(`outputs the collected input data of the size`, async () => {
+    const size = 3;
+    const outputs: Array<Array<number>> = [];
+    await pipeline(
+      Readable.from([1, 2, 3, 4, 5]),
+      new BufferStream({ size: size }, { objectMode: true }),
+      new ToArrayStream({ target: outputs }, { objectMode: true }),
+    );
+
+    expect(outputs).toEqual([
+      [1, 2, 3],
+      [4, 5],
+    ]);
+  });
+
+  it(`outputs the collected input data of the size with encoding`, async () => {
+    const size = 3;
+    const outputs: Array<Array<{ chunk: any; encoding: string }>> = [];
+    await pipeline(
+      Readable.from([`a`, `b`, `c`, `d`, `e`]),
+      new BufferStream(
+        { size: size, includeEncoding: true },
+        { objectMode: true },
+      ),
+      new ToArrayStream({ target: outputs }, { objectMode: true }),
+    );
+
+    expect(outputs).toEqual([
+      [
+        { chunk: `a`, encoding: `utf8` },
+        { chunk: `b`, encoding: `utf8` },
+        { chunk: `c`, encoding: `utf8` },
+      ],
+      [
+        { chunk: `d`, encoding: `utf8` },
+        { chunk: `e`, encoding: `utf8` },
+      ],
+    ]);
+  });
+
+  it(`outputs the collected input data after waitMs even if the number is not enough.`, async () => {
+    const size = 3;
+    const waitMs = 100;
+    const outputs: Array<Array<number>> = [];
+    await pipeline(
+      Readable.from(
+        (async function* () {
+          yield 1;
+          yield 2;
+          await delay(waitMs * 2);
+          yield 3;
+          await delay(waitMs * 2);
+          yield 4;
+          yield 5;
+        })(),
+      ),
+      new BufferStream({ size: size, waitMs: waitMs }, { objectMode: true }),
+      new ToArrayStream({ target: outputs }, { objectMode: true }),
+    );
+
+    expect(outputs).toEqual([[1, 2], [3], [4, 5]]);
+  });
+});
