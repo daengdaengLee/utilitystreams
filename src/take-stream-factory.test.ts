3cc24a66e5a93a1b6f44400fd80950a5dd309c74
daengdaengLee <gunho1020@gmail.com>
2023-08-15 15:12:49 +0900

    feat: takeStreamFactory 함수 curry 지원

--- src/take-stream-factory.test.ts
+++ src/take-stream-factory.test.ts
@@ -4,45 +4,68 @@ import { takeStreamFactory } from "./take-stream-factory.js";
 import { ToArrayStream } from "./to-array-stream.js";
 
 describe(`TakeStreamFactory Test`, () => {
-  it(`outputs only the number you set and exits.`, async () => {
-    const n = 2;
+  const n = 2;
+  const inputs = [1, 2, 3, 4, 5, 6];
 
-    const inputs = [
-      Buffer.from([1]),
-      Buffer.from([2]),
-      Buffer.from([3]),
-      Buffer.from([4]),
-      Buffer.from([5]),
-      Buffer.from([6]),
-    ];
-    const outputs: Array<Buffer> = [];
-    await pipeline(
-      takeStreamFactory({ n: n }, Readable.from(inputs)),
-      new ToArrayStream({ target: outputs }),
-    );
+  describe(`outputs only the number you set and exits.`, () => {
+    it(`no currey version`, async () => {
+      const outputs: Array<number> = [];
+      await pipeline(
+        takeStreamFactory({ n: n }, Readable.from(inputs)),
+        new ToArrayStream({ target: outputs }, { objectMode: true }),
+      );
 
-    expect(outputs).toEqual(inputs.slice(0, n));
+      expect(outputs).toEqual(inputs.slice(0, n));
+    });
+
+    it(`currey version`, async () => {
+      const outputs: Array<number> = [];
+      await pipeline(
+        takeStreamFactory({ n: n })(Readable.from(inputs)),
+        new ToArrayStream({ target: outputs }, { objectMode: true }),
+      );
+
+      expect(outputs).toEqual(inputs.slice(0, n));
+    });
   });
 
-  it(`close source stream after at most n data yields`, async () => {
-    const n = 2;
+  describe(`close source stream after at most n data yields`, () => {
+    it(`no curry version`, async () => {
+      const sourceOutputs: Array<number> = [];
+      let sourceDone = false;
+      const sourceStream = Readable.from(inputs);
+      sourceStream.on(`data`, (value) => {
+        sourceOutputs.push(value);
+      });
+      sourceStream.on(`close`, () => {
+        sourceDone = true;
+      });
+      await pipeline(
+        takeStreamFactory({ n: n }, sourceStream),
+        new PassThrough({ objectMode: true }),
+      );
 
-    const inputs = [1, 2, 3, 4, 5, 6];
-    const sourceOutputs: Array<number> = [];
-    let sourceDone = false;
-    const sourceStream = Readable.from(inputs);
-    sourceStream.on(`data`, (value) => {
-      sourceOutputs.push(value);
+      expect(sourceOutputs).toEqual(inputs.slice(0, n));
+      expect(sourceDone).toBeTruthy();
     });
-    sourceStream.on(`close`, () => {
-      sourceDone = true;
-    });
-    await pipeline(
-      takeStreamFactory({ n: n }, sourceStream),
-      new PassThrough({ objectMode: true }),
-    );
 
-    expect(sourceOutputs).toEqual(inputs.slice(0, n));
-    expect(sourceDone).toBeTruthy();
+    it(`curry version`, async () => {
+      const sourceOutputs: Array<number> = [];
+      let sourceDone = false;
+      const sourceStream = Readable.from(inputs);
+      sourceStream.on(`data`, (value) => {
+        sourceOutputs.push(value);
+      });
+      sourceStream.on(`close`, () => {
+        sourceDone = true;
+      });
+      await pipeline(
+        takeStreamFactory({ n: n })(sourceStream),
+        new PassThrough({ objectMode: true }),
+      );
+
+      expect(sourceOutputs).toEqual(inputs.slice(0, n));
+      expect(sourceDone).toBeTruthy();
+    });
   });
 });
--- src/take-stream-factory.ts
+++ src/take-stream-factory.ts
@@ -1,7 +1,31 @@
-export async function* takeStreamFactory<T>(
-  options: { n: number },
-  stream: Iterable<T> | AsyncIterable<T>,
-) {
+type Options = { n: number };
+type Stream<T> = Iterable<T> | AsyncIterable<T>;
+type Result<T> = AsyncGenerator<T>;
+
+export function takeStreamFactory<T>(
+  options: Options,
+): (stream: Stream<T>) => Result<T>;
+export function takeStreamFactory<T>(
+  options: Options,
+  stream: Stream<T>,
+): Result<T>;
+export function takeStreamFactory<T>(
+  options: Options,
+  stream?: Stream<T>,
+): ((stream: Stream<T>) => Result<T>) | Result<T> {
+  if (stream != null) {
+    return _takeStreamFactory(options, stream);
+  }
+
+  return (stream: Stream<T>): Result<T> => {
+    return _takeStreamFactory(options, stream);
+  };
+}
+
+async function* _takeStreamFactory<T>(
+  options: Options,
+  stream: Stream<T>,
+): Result<T> {
   let i = 0;
   for await (const value of stream) {
     if (i >= options.n) {
