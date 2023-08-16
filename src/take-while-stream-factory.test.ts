47dcf9b0f909961bf90c2fe877680be145a837b3
daengdaengLee <gunho1020@gmail.com>
2023-08-16 13:50:00 +0900

    refactor: 테스트 공통 함수 분리

--- src/take-while-stream-factory.test.ts
+++ src/take-while-stream-factory.test.ts
@@ -1,28 +1,11 @@
 import { Readable } from "node:stream";
 import { pipeline } from "node:stream/promises";
 import { takeWhileStreamFactory } from "./take-while-stream-factory.js";
-import { asyncInputs } from "./test-util/async-inputs.js";
 import { sourceStreamCloseTestTemplate } from "./test-util/source-stream-close-test-template.js";
-import { syncInputs } from "./test-util/sync-inputs.js";
+import { expected, testCases } from "./test-util/take-while-test-cases.js";
 import { ToArrayStream } from "./to-array-stream.js";
-import { delay } from "./util.js";
 
 describe(`takeWhileStreamFactory Test`, () => {
-  const syncF = (n: number): boolean => {
-    return n < 5;
-  };
-  const asyncF = async (n: number): Promise<boolean> => {
-    await delay(100);
-    return syncF(n);
-  };
-  const expected: Array<number> = [1, 2, 3, 4];
-  const testCases = [
-    { name: `sync data + sync predicate`, f: syncF, inputs: syncInputs },
-    { name: `sync data + async predicate`, f: asyncF, inputs: syncInputs },
-    { name: `async data + sync predicate`, f: syncF, inputs: asyncInputs },
-    { name: `async data + async predicate`, f: asyncF, inputs: asyncInputs },
-  ];
-
   describe(`outputs while the predicate function returns true and exits.`, () => {
     for (const { name, f, inputs } of testCases) {
       describe(name, () => {
--- src/take-while-stream.test.ts
+++ src/take-while-stream.test.ts
@@ -1,142 +1,39 @@
 import { Readable } from "node:stream";
 import { pipeline } from "node:stream/promises";
 import { TakeWhileStream } from "./take-while-stream.js";
-import { asyncInputs } from "./test-util/async-inputs.js";
-import { syncInputs } from "./test-util/sync-inputs.js";
+import { expected, testCases } from "./test-util/take-while-test-cases.js";
 import { ToArrayStream } from "./to-array-stream.js";
-import { delay } from "./util.js";
 
 describe(`TakeWhileStream Test`, () => {
-  const syncF = (n: number): boolean => {
-    return n < 5;
-  };
-  const asyncF = async (n: number): Promise<boolean> => {
-    await delay(100);
-    return syncF(n);
-  };
-  const expected: Array<number> = [1, 2, 3, 4];
-
   describe(`outputs while the predicate function returns true and exits.`, () => {
-    it(`sync data + sync predicate`, async () => {
-      const outputs: Array<number> = [];
-      await pipeline(
-        Readable.from(syncInputs()),
-        new TakeWhileStream({ f: syncF }, { objectMode: true }),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-    });
-
-    it(`sync data + async predicate`, async () => {
-      const outputs: Array<number> = [];
-      await pipeline(
-        Readable.from(syncInputs()),
-        new TakeWhileStream({ f: asyncF }, { objectMode: true }),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-    });
-
-    it(`async data + sync predicate`, async () => {
-      const outputs: Array<number> = [];
-      await pipeline(
-        Readable.from(asyncInputs()),
-        new TakeWhileStream({ f: syncF }, { objectMode: true }),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-    });
-
-    it(`async data + async predicate`, async () => {
-      const outputs: Array<number> = [];
-      await pipeline(
-        Readable.from(asyncInputs()),
-        new TakeWhileStream({ f: asyncF }, { objectMode: true }),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-    });
+    for (const { name, f, inputs } of testCases) {
+      it(name, async () => {
+        const outputs: Array<number> = [];
+        await pipeline(
+          Readable.from(inputs()),
+          new TakeWhileStream({ f: f }, { objectMode: true }),
+          new ToArrayStream({ target: outputs }, { objectMode: true }),
+        );
+        expect(outputs).toEqual(expected);
+      });
+    }
   });
 
   describe(`done() when the predicate function returns false first or there is no more data.`, () => {
-    it(`sync data + sync predicate`, async () => {
-      const outputs: Array<number> = [];
-      let dataWhenDone = null;
-      await pipeline(
-        Readable.from(syncInputs()),
-        new TakeWhileStream(
-          {
-            f: syncF,
-            done: () => {
-              dataWhenDone = outputs.at(-1);
-            },
-          },
-          { objectMode: true },
-        ),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-      expect(dataWhenDone).toEqual(expected.at(-1));
-    });
-
-    it(`sync data + async predicate`, async () => {
-      const outputs: Array<number> = [];
-      let dataWhenDone = null;
-      await pipeline(
-        Readable.from(syncInputs()),
-        new TakeWhileStream(
-          {
-            f: asyncF,
-            done: () => {
-              dataWhenDone = outputs.at(-1);
-            },
-          },
-          { objectMode: true },
-        ),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-      expect(dataWhenDone).toEqual(expected.at(-1));
-    });
-
-    it(`async data + sync predicate`, async () => {
-      const outputs: Array<number> = [];
-      let dataWhenDone = null;
-      await pipeline(
-        Readable.from(asyncInputs()),
-        new TakeWhileStream(
-          {
-            f: syncF,
-            done: () => {
-              dataWhenDone = outputs.at(-1);
-            },
-          },
-          { objectMode: true },
-        ),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-      expect(dataWhenDone).toEqual(expected.at(-1));
-    });
-
-    it(`async data + async predicate`, async () => {
-      const outputs: Array<number> = [];
-      let dataWhenDone = null;
-      await pipeline(
-        Readable.from(asyncInputs()),
-        new TakeWhileStream(
-          {
-            f: asyncF,
-            done: () => {
-              dataWhenDone = outputs.at(-1);
-            },
-          },
-          { objectMode: true },
-        ),
-        new ToArrayStream({ target: outputs }, { objectMode: true }),
-      );
-      expect(outputs).toEqual(expected);
-      expect(dataWhenDone).toEqual(expected.at(-1));
-    });
+    for (const { name, f, inputs } of testCases) {
+      it(name, async () => {
+        const outputs: Array<number> = [];
+        let dataWhenDone = null;
+        const done = (): void => {
+          dataWhenDone = outputs.at(-1);
+        };
+        await pipeline(
+          Readable.from(inputs()),
+          new TakeWhileStream({ f: f, done: done }, { objectMode: true }),
+          new ToArrayStream({ target: outputs }, { objectMode: true }),
+        );
+        expect(dataWhenDone).toEqual(expected.at(-1));
+      });
+    }
   });
 });
