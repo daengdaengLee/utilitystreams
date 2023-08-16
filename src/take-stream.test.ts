df4b0fb8d76b35d0bdac0ee05845a8903b575d60
daengdaengLee <gunho1020@gmail.com>
2023-08-16 13:44:45 +0900

    refactor: 테스트 공통 함수 분리

--- src/take-stream.test.ts
+++ src/take-stream.test.ts
@@ -1,55 +1,39 @@
 import { Readable } from "node:stream";
 import { pipeline } from "node:stream/promises";
 import { TakeStream } from "./take-stream.js";
+import { n, expected, testCases } from "./test-util/take-test-cases.js";
 import { ToArrayStream } from "./to-array-stream.js";
 
 describe(`TakeStream Test`, () => {
-  it(`outputs only the number you set and exits.`, async () => {
-    const n = 2;
-
-    const inputs = [
-      Buffer.from([1]),
-      Buffer.from([2]),
-      Buffer.from([3]),
-      Buffer.from([4]),
-      Buffer.from([5]),
-      Buffer.from([6]),
-    ];
-    const outputs: Array<Buffer> = [];
-    await pipeline(
-      Readable.from(inputs),
-      new TakeStream({ n: n }),
-      new ToArrayStream({ target: outputs }),
-    );
-
-    expect(outputs).toEqual(inputs.slice(0, n));
+  describe(`outputs only the number you set and exits.`, () => {
+    for (const { name, inputs } of testCases) {
+      it(name, async () => {
+        const outputs: Array<number> = [];
+        await pipeline(
+          Readable.from(inputs()),
+          new TakeStream({ n: n }, { objectMode: true }),
+          new ToArrayStream({ target: outputs }, { objectMode: true }),
+        );
+        expect(outputs).toEqual(expected);
+      });
+    }
   });
 
-  it(`done() when n-th data transformed.`, async () => {
-    const n = 2;
-
-    const inputs = [
-      Buffer.from([1]),
-      Buffer.from([2]),
-      Buffer.from([3]),
-      Buffer.from([4]),
-      Buffer.from([5]),
-      Buffer.from([6]),
-    ];
-    const outputs: Array<Buffer> = [];
-    let dataWhenDone = null;
-    await pipeline(
-      Readable.from(inputs),
-      new TakeStream({
-        n: n,
-        done: () => {
+  describe(`done() when n-th data transformed.`, () => {
+    for (const { name, inputs } of testCases) {
+      it(name, async () => {
+        const outputs: Array<number> = [];
+        let dataWhenDone = null;
+        const done = (): void => {
           dataWhenDone = outputs.at(-1);
-        },
-      }),
-      new ToArrayStream({ target: outputs }),
-    );
-
-    expect(outputs).toEqual(inputs.slice(0, n));
-    expect(dataWhenDone).toEqual(inputs.slice(0, n).at(-1));
+        };
+        await pipeline(
+          Readable.from(inputs()),
+          new TakeStream({ n: n, done: done }, { objectMode: true }),
+          new ToArrayStream({ target: outputs }, { objectMode: true }),
+        );
+        expect(dataWhenDone).toEqual(expected.at(-1));
+      });
+    }
   });
 });
