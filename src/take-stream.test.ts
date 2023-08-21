01c92017622cd1e53fcbeffaff10d5124a5c9922
daengdaengLee <gunho1020@gmail.com>
2023-08-21 17:43:28 +0900

    test: 깨진 테스트 수정

--- src/take-stream.test.ts
+++ src/take-stream.test.ts
@@ -1,7 +1,7 @@
-import { Readable } from "node:stream";
+import { PassThrough, Readable, TransformCallback } from "node:stream";
 import { pipeline } from "node:stream/promises";
 import { TakeStream } from "./take-stream.js";
-import { n, expected, testCases } from "./test-util/take-test-cases.js";
+import { expected, n, testCases } from "./test-util/take-test-cases.js";
 import { ToArrayStream } from "./to-array-stream.js";
 
 describe(`TakeStream Test`, () => {
@@ -22,15 +22,26 @@ describe(`TakeStream Test`, () => {
   describe(`done() when n-th data transformed.`, () => {
     for (const { name, inputs } of testCases) {
       it(name, async () => {
-        const outputs: Array<number> = [];
+        let emittedData: any = null;
         let dataWhenDone = null;
         const done = (): void => {
-          dataWhenDone = outputs.at(-1);
+          dataWhenDone = emittedData;
         };
         await pipeline(
           Readable.from(inputs()),
           new TakeStream({ n: n, done: done }, { objectMode: true }),
-          new ToArrayStream({ target: outputs }, { objectMode: true }),
+          new PassThrough({
+            transform(
+              chunk: any,
+              encoding: BufferEncoding,
+              callback: TransformCallback,
+            ) {
+              emittedData = chunk;
+              this.push(chunk, encoding);
+              callback();
+            },
+            objectMode: true,
+          }),
         );
         expect(dataWhenDone).toEqual(expected.at(-1));
       });
