8b3cb75cdf726b60af376e51ee8d9a7da2abcd07
daengdaengLee <gunho1020@gmail.com>
2023-09-11 12:11:03 +0900

    test: 테스트 항목 초안 작성

---
+++ src/tap-stream.test.ts
@@ -0,0 +1,26 @@
+describe(`TapStream Test`, () => {
+  describe(`call consumer for each input.`, () => {
+    // @TODO
+    it(`placeholder`, () => {});
+  });
+
+  describe(`await before consumer call finished.`, () => {
+    // @TODO
+    it(`placeholder`, () => {});
+  });
+
+  describe(`not await before consumer call finished.`, () => {
+    // @TODO
+    it(`placeholder`, () => {});
+  });
+
+  describe(`not propagate error`, () => {
+    // @TODO
+    it(`placeholder`, () => {});
+  });
+
+  describe(`propagate error`, () => {
+    // @TODO
+    it(`placeholder`, () => {});
+  });
+});
