3eb8df0cc203a8d48eb5a78f3ecadc305ade400d
daengdaengLee <gunho1020@gmail.com>
2023-09-25 18:01:17 +0900

    test: 기본 성공 테스트 작성

--- src/tap-stream.test.ts
+++ src/tap-stream.test.ts
@@ -1,25 +1,60 @@
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { TapStream } from "./tap-stream";
+import { asyncInputs } from "./test-util/async-inputs";
+import { syncInputs } from "./test-util/sync-inputs";
+import { ToArrayStream } from "./to-array-stream";
+import { delay } from "./util";
+
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const syncConsumerFactory = (outputs: Array<number>) => (value: number) => {
+  outputs.push(value);
+};
+const asyncConsumerFactory =
+  (outputs: Array<number>) => async (value: number) => {
+    await delay(10);
+    outputs.push(value);
+  };
+const testCases = [
+  {
+    name: `sync data + sync consumer`,
+    inputs: syncInputs,
+    consumerFactory: syncConsumerFactory,
+  },
+  {
+    name: `sync data + async consumer`,
+    inputs: syncInputs,
+    consumerFactory: asyncConsumerFactory,
+  },
+  {
+    name: `async data + sync consumer`,
+    inputs: asyncInputs,
+    consumerFactory: syncConsumerFactory,
+  },
+  {
+    name: `async data + async consumer`,
+    inputs: asyncInputs,
+    consumerFactory: asyncConsumerFactory,
+  },
+];
+
 describe(`TapStream Test`, () => {
   describe(`call consumer for each input.`, () => {
-    // @TODO
-    it(`placeholder`, () => {});
-  });
-
-  describe(`await before consumer call finished.`, () => {
-    // @TODO
-    it(`placeholder`, () => {});
-  });
-
-  describe(`not await before consumer call finished.`, () => {
-    // @TODO
-    it(`placeholder`, () => {});
+    for (const { name, inputs, consumerFactory } of testCases) {
+      it(name, async () => {
+        const outputs: Array<number> = [];
+        const consumer = consumerFactory(outputs);
+        await pipeline(
+          Readable.from(inputs()),
+          new TapStream({ f: consumer }, { objectMode: true }),
+          new ToArrayStream({}, { objectMode: true }),
+        );
+        expect(outputs).toEqual(expected);
+      });
+    }
   });
 
   describe(`not propagate error`, () => {
-    // @TODO
-    it(`placeholder`, () => {});
-  });
-
-  describe(`propagate error`, () => {
     // @TODO
     it(`placeholder`, () => {});
   });
